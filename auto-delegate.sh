3513c0d1f3b81ee4898d605d803aac755395a4ac
Leopold Joy <leo@leopoldjoy.com>
2018-12-18 07:43:46 -0500

    Implement temporary workaround for calling withdraw-rewards to update STAKE balance

--- auto-delegate.sh
+++ auto-delegate.sh
@@ -3,10 +3,13 @@ read -s password
 
 while true
 do
-  amount_steak=$(sudo -u gaiad /opt/go/bin/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=genki-3001 --trust-node=true | jq -r '.value.coins[0].amount')
-	if [[ $amount_steak > 0 && $amount_steak != "null" ]]; then
-	        echo "About to stake ${amount_steak} steak"
-	        echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=stake-capital-genki-3001-key --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=genki-3001
-	fi
-  sleep 1h
+  echo "About to withdraw rewards (for validator and delegator)"
+  echo "${password}" | sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes --trust-node
+  sleep 30m
+  amount_steak=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=game_of_stakes --trust-node=true | jq -r '.value.coins[0].amount')
+  if [[ $amount_steak > 0 && $amount_steak != "null" ]]; then
+    echo "About to stake ${amount_steak} steak"
+    echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes
+  fi
+  sleep 30m
 done
