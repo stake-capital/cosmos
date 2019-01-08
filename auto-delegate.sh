d5471cea3762519f3cd8941d0a64e053bf6b07cb
Julien Bouteloup <julien.bouteloup@gmail.com>
2019-01-09 00:39:44 +0100

    Better quick logic + timer
    Add a better logic to consume less resources

--- auto-delegate.sh
+++ auto-delegate.sh
@@ -5,21 +5,26 @@ echo "To kill this script in the future, simply run this command: kill -9 $$"
 
 while true
 do
-  echo "About to withdraw rewards (for validator and delegator)"
-  echo "${password}" | sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --async --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_3 --trust-node
   num_unconfirmed_txs=$(curl -v --silent curl localhost:26657/num_unconfirmed_txs --stderr - | grep n_txs | cut -c15)
-  echo "Number of unconfirmed tx's: $num_unconfirmed_txs"
-  if [[ $num_unconfirmed_txs > 9 ]]; then
-    echo "Clearing $num_unconfirmed_txs transactions"
-    curl localhost:26657/unsafe_flush_mempool
+  if [[ $num_unconfirmed_txs > 0 && !($towait > 0)]]
+  then
+      echo "Waiting for Delegate TX to be taken..."
+      towait=1
+      sleep 5m
+  else
+    towait=0
+    echo "Previous withdraw was successfully delegated."
+    echo "About to withdraw new rewards."
+    echo "${password}" | sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --async --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_3 --trust-node
+    amount_steak=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].amount')
+    if [[ $amount_steak > 0 && $amount_steak != "null" ]]; then
+      duration=$(( SECONDS - start ))
+      echo "BOOM! Previous delegation went through in $duration seconds"
+      echo "About to stake ${amount_steak} stakes"
+      echo "Estimated gas:"
+      echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --dry-run
+      echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --fee="200000photinos" | start=$SECONDS
+    fi
+    sleep 5m
   fi
-  amount_steak=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].amount')
-  if [[ $amount_steak > 0 && $amount_steak != "null" ]]; then
-    echo "About to stake ${amount_steak} steak"
-    echo "Estimated gas:"
-    echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --dry-run
-    echo "Running tx stake delegate:"
-    echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --fee="200000photinos"
-  fi
-  sleep 5m
 done
