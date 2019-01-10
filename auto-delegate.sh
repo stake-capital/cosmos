b06bc1e762781494e2ab53edac9f20c6afde5951
Julien Bouteloup <julien.bouteloup@gmail.com>
2019-01-10 01:08:23 +0100

    Fix condition and logic issue

--- auto-delegate.sh
+++ auto-delegate.sh
@@ -9,24 +9,28 @@ echo "To kill this script in the future, simply run this command: kill -9 $$"
 while true
 do
   num_unconfirmed_txs=$(curl -v --silent curl localhost:26657/num_unconfirmed_txs --stderr - | grep n_txs | cut -c15)
+  echo "Number of unconfirmed txs: $num_unconfirmed_txs"
   if [[ $num_unconfirmed_txs > 0 && !($towait > 0)]]
   then
-      echo "Waiting for Delegate TX to be taken..."
+      echo "There is a TX in the mempool"
+      echo "..."
       towait=1
       sleep 10s
   else
     towait=0
-    echo "Previous withdraw was successfully delegated."
-    echo "About to withdraw new rewards."
-    echo "${password}" | sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --async --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_3 --trust-node
     amount_steak=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=game_of_stakes_3 --trust-node=true | jq -r '.value.coins[0].amount')
-    if [[ $amount_steak > 0 && $amount_steak != "null" && !($amount_steak > 10000000)]]; then
-      duration=$(( SECONDS - start ))
-      echo "BOOM! Previous delegation went through in $duration seconds"
-      echo "About to stake ${amount_steak} stakes"
-      #echo "Estimated gas:"
-      #echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --dry-run
-      echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3 --fee="22STAKE" | start=$SECONDS
+    echo "Number of stakes: ${amount_steak}"
+    if [[ $amount_steak > 0 && $amount_steak != "null" && ($amount_steak -lt 10000000)]]
+      then
+        duration=$(( SECONDS - start ))
+        echo "$duration seconds since last TX call"
+        #echo "Estimated gas:"
+        #echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3  --trust-node=true --dry-run
+        echo "Sending transaction to delegate ${amount_steak} stakes"
+        echo "${password}" | sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${amount_steak}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3 --fee="22STAKE" | start=$SECONDS
+      else
+        echo "Sending transaction to withdraw stakes"
+        echo "${password}" | sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --async --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_3 --trust-node
     fi
     sleep 5m
   fi
