ff1359fb579fe7816624b90e35fbd9ac7b7edd6f
Leopold Joy <leo@leopoldjoy.com>
2019-01-21 13:02:44 -0500

    implement fix for bug triggered during repetitive overshoot estimations

--- auto-delegate.sh
+++ auto-delegate.sh
@@ -5,9 +5,18 @@
 #       which will generate the above file (at /etc/systemd/system/myservice.service.d/myenv.conf)
 
 echo "Setting (fake) initial historical interval stake earnings..."
-last_10_interval_earnings[0]=260
+last_10_interval_earnings[0]=303
+last_10_interval_earnings[1]=319
+last_10_interval_earnings[2]=426
+last_10_interval_earnings[3]=426
+last_10_interval_earnings[4]=336
+last_10_interval_earnings[5]=745
+last_10_interval_earnings[6]=380
+last_10_interval_earnings[7]=380
+last_10_interval_earnings[8]=380
+last_10_interval_earnings[9]=380
 
-remaining_stake_from_delegation_before_last=0
+remaining_stake_from_delegation_before_last=447
 
 combo_txn_file="./txn.json"
 
@@ -47,33 +56,26 @@ do
     then
       # In this case we overshot the actual accrued STAKE since our estimate was just below the actual amount.
       echo "Overshot (didn't effectively delegate) actual accrued STAKE for last estimation."
-      # To determine the "corrected" amount for the last delegation, we first check if the amount of stake
-      # from the delegation attempt before last is equal to the amount of stake left from our last delegation
-      # attempt (since if they're equal it means that the last transaction failed):
-      if [[($remaining_stake_from_delegation_before_last -eq $remaining_stake_left_from_last_delegation)]]
+      # As long as the remaining stake from the last delegation is less than the remaining stake from the delegation
+      # before that, we can calculate the "corrected" amount of STAKE before the last delegation. Otherwise we must
+      # simply reduce the current minimum value by 5% (since we do not have information to know how much to reduce
+      # the min by due to the last withdraw transaction failing).
+      if [[($remaining_stake_from_delegation_before_last -lt  $remaining_stake_left_from_last_delegation)]]
       then
-        # Because the last transaction failed, we have no new information about how much stake was left from the
-        # last transaction since the withdraw call didn't go through (hence why they're equal). Thus, since we do
-        # not have sufficient informaton and only know that we overshow the target, we simply correct by reducing
-        # the current minimum value by 10%:
-        corrected_total_accrued_before_last_delegation=$(echo $last_min_from_last_10*9/10 | bc)
-        echo "They were equal!"
-        echo "Corrected total = (last 10 min * 0.9)"
-      # We now check if the amount of stake from the delegation attempt before last is greater than the amount of
-      # stake left from our last delegation attempt
-      elif [[($remaining_stake_from_delegation_before_last -gt  $remaining_stake_left_from_last_delegation)]]
-      then
-          # The total amount of stake left from our last delegation attempt represents the "corrected" amount
-          # for this last delegation.
-          corrected_total_accrued_before_last_delegation=$remaining_stake_left_from_last_delegation
-          echo "Corrected total = (stake left from last delegation)"
+        # The total amount of stake left from our last delegation attempt, minus the STAKE left over from the
+        # delegation before that, represents the "corrected" amount for this last delegation.
+        corrected_total_accrued_before_last_delegation=$((remaining_stake_left_from_last_delegation-remaining_stake_from_delegation_before_last))
+        echo "Corrected total = (stake left from delegation before last) - (stake left from last delegation)"
       else
-          # The total amount of stake left from our last delegation attempt, minus the STAKE left over from the
-          # delegation before that, represents the "corrected" amount for this last delegation.
-          corrected_total_accrued_before_last_delegation=$((remaining_stake_left_from_last_delegation-remaining_stake_from_delegation_before_last))
-          echo "Corrected total = (stake left from delegation before last) - (stake left from last delegation)"
+        # Because the last transaction failed, we have no new information about how much stake was left from the
+        # last transaction since the withdraw call didn't go through. Thus, since we do
+        # not have sufficient informaton and only know that we overshot the target, we simply correct by reducing
+        # the current minimum value by 5%:
+        corrected_total_accrued_before_last_delegation=$(echo $last_min_from_last_10*19/20 | bc)
+        echo "They were equal!"
+        echo "Corrected total = (last 10 min * 0.95)"
       fi
-      echo "This is how much STAKE there was: $corrected_total_accrued_before_last_delegation"
+      echo "Corrected value: $corrected_total_accrued_before_last_delegation"
     else
       # In this case we had a good estimation (marginally undershot the actual amount of accrued STAKE).
       # Now we calculate the actual, "corrected", amount from our last delegation:
@@ -95,11 +97,20 @@ do
     remaining_stake_from_delegation_before_last=$remaining_stake_left_from_last_delegation
 
     echo "Updated 10 most recent interval earnings:"
-    printf '%s, ' "${last_10_interval_earnings[@]}\n"
+    printf '%s, ' "${last_10_interval_earnings[@]}"
+    echo ""
     echo "Updated minimum value (from last 10): $new_min_from_last_10"
     echo "Extra STAKE to delegate with this transaction (from under-estimation on last transaction): $remaining_stake_left_from_last_delegation"
 
-    total_to_delegate=$((new_min_from_last_10+remaining_stake_left_from_last_delegation))
+    # Check if the last withdraw attempt was did not work; if so, multiply the new_min_from_last_10 by two,
+    # since two windows of delegation will have been missed.
+    if [[($remaining_stake_left_from_last_delegation -ge $last_min_from_last_10)]] && [[($remaining_stake_from_delegation_before_last -eq $remaining_stake_left_from_last_delegation)]]
+    then
+      echo "(Multiply total new_min_from_last_10 by two since we will have accrued two delegation windows worth of STAKE.)"
+      total_to_delegate=$(((new_min_from_last_10*2)+remaining_stake_left_from_last_delegation))
+    else
+      total_to_delegate=$((new_min_from_last_10+remaining_stake_left_from_last_delegation))
+    fi
 
     echo "About to withdraw (all STAKE) and delegate: $total_to_delegate STAKE"
 
@@ -107,7 +118,7 @@ do
     withdrawal_txn=$(sudo /opt/go/bin/gaiacli --home=/opt/gaiacli tx dist withdraw-rewards --is-validator --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_3 --fee=1000photinos --trust-node --generate-only)
 
     # Create delegation transaction and save withdrawal message to variable
-    delegation_txn=$(sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --async --home=/opt/gaiacli --amount=${total_to_delegate}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3 --fee=1000photinos --generate-only)
+    delegation_txn=$(sudo -u gaiad /opt/go/bin/gaiacli tx stake delegate --home=/opt/gaiacli --amount=${total_to_delegate}STAKE --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_3 --fee=1000photinos --generate-only)
     delegation_msg=$(echo $delegation_txn | jq -r '.value.msg[0]')
 
     # Combine the delegation message with the withdrawal transaction to create the final transaction (with both messages)
