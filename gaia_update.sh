63404cb29d5ae3fb5b3b7897b0bb161af934a5a8
Leopold Joy <leo@leopoldjoy.com>
2019-02-02 03:15:01 -0500

    update gaia_update.sh script for gaia 0.30 changes

--- gaia_update.sh
+++ gaia_update.sh
@@ -3,16 +3,15 @@
 # Run: ./gaia_update.sh
 
 # Update cosmos sdk repo
-go get github.com/cosmos/cosmos-sdk
 cd $HOME/go/src/github.com/cosmos/cosmos-sdk
 git fetch --all
-git checkout -f v0.28.0
+git checkout -f v0.30.0
 
 # Export GO env.
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 
 # Install 
-make get_tools && make get_vendor_deps && make install
+make tools install
 sudo rm -rf /opt/go/bin/gaia*
 sudo cp $HOME/go/bin/gaia* /opt/go/bin/
