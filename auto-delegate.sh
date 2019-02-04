e440238474d25365b469b445e9943d1eb57bae5b
Leopold Joy <leo@leopoldjoy.com>
2019-02-04 05:00:01 -0500

    implement delayed generation of withdrawal/delegation transaction until about to be proposer (within 5 blocks)

--- auto-delegate.sh
+++ auto-delegate.sh
@@ -15,45 +15,68 @@ while true
 do
   num_unconfirmed_txs=$(curl -v --silent curl localhost:26657/num_unconfirmed_txs --stderr - | grep n_txs | cut -c15)
 
+  # If there's a transaction in the mempool it means that we've already issued a delegation for the next round where we're proposer,
+  # thus no need to do anything.
   echo "Number of unconfirmed txs: $num_unconfirmed_txs"
-  if [[ ($num_unconfirmed_txs -gt 20)]]
+  if [[ ($num_unconfirmed_txs -gt 0) ]]
   then
-    echo "There are more than 20 TX in the mempool"
+    echo "There is more than 1 TX in the mempool, waiting 1 minute before creating additional transactions..."
     echo "..."
     sleep 1m
   else
 
-    amount_to_delegate=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query distr commission cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --trust-node --output json | jq -r '.[1].amount' | awk '{printf("%d\n",$1)}')
+    # Create an array that is empty if we're not proposer within the next 5 blocks OR has one element if we are
+    array_to_check_if_proposer_soon=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query tendermint-validator-set --chain-id game_of_stakes_5 --trust-node | jq -r '.validators[]|{proposer_priority: (.proposer_priority)|tonumber, pub_key: (.pub_key)}' | jq --slurp '.' | jq -r '.=sort_by(.proposer_priority)|reverse|.[0:5]|map(select(.pub_key == "cosmosvalconspub1zcjduepq2ctc8pm0kzsuuj53atr9d5eruqed2fun3rcgntjrk3gd7jumq5qq7xcepm"))')
 
-    echo "There is $amount_to_delegate stake to delegate. Generating transaction..."
+    # Check if we're proposer soon (within next 5 blocks)
+    if [ $array_to_check_if_proposer_soon = "[]" ]
+    then
+      echo "We're not the proposer soon (within 5 blocks), so we wait to create the txn (so that the stake estimation is more accurate)..."
+      # Since block times are ~6 seconds, 5 blocks * 6 seconds  = 30 seconds. We now wait 25 seconds (5 extra seconds to be safe since block time can vary)
+      sleep 25s
+    else
+      new_stake_to_delegate=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query distr commission cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --trust-node --output json | jq -r '.[1].amount' | awk '{printf("%d\n",$1)}')
 
-    # Create withdrawal transaction
-    withdrawal_txn=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx distr withdraw-rewards --is-validator --async --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_5 --trust-node --generate-only --gas=154250)
+      echo "There is $new_stake_to_delegate new stake to delegate (not yet withdrawn)"
 
-    # Create delegation transaction and save withdrawal message to variable
-    delegation_txn=$(sudo -u gaiad /opt/go/bin/gaiacli tx staking delegate --home=/opt/gaiacli --amount=${amount_to_delegate}stake --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_5 --generate-only --gas=154250)
-    delegation_msg=$(echo $delegation_txn | jq -r '.value.msg[0]')
+      remaining_from_past_delegations=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli query account cosmos1844lltc96kxkm5mq03my90se4cdssewmh77shu --chain-id=game_of_stakes_5 --trust-node=true --output json | jq -r '.value.BaseVestingAccount.BaseAccount.coins[1].amount')
 
-    # Combine the delegation message with the withdrawal transaction to create the final transaction (with both messages)
-    withdrawal_and_delegation_txn=$(echo $withdrawal_txn | jq ".value.msg[1] |= . + $delegation_msg")
+      echo "There is $remaining_from_past_delegations stake remaining from past delegation (already withdrawn but wasn't delegated)"
 
-    echo "Saving generated withdrawal/delegation transaction to $combo_txn_file..."
+      total=$((new_stake_to_delegate + remaining_from_past_delegations))
 
-    # Write combined transaction to file
-    echo "$withdrawal_and_delegation_txn" | sudo tee $combo_txn_file
+      echo "Total to delegate now: $total"
 
-    # Sign combined transaction
-    signed_txn=$(echo "${GAIA_KEY}" | sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx sign $combo_txn_file --trust-node --account-number 0 --name game-of-stake-key-validator4 --gas=154250 --chain-id game_of_stakes_5 --from=game-of-stake-key-validator4)
+      echo "Generating transaction..."
 
-    echo "Transaction signed! Saving signed transaction to $combo_txn_file..."
+      # Create withdrawal transaction
+      withdrawal_txn=$(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx distr withdraw-rewards --is-validator --async --from=game-of-stake-key-validator4 --chain-id=game_of_stakes_5 --trust-node --generate-only --gas=154250)
 
-    # Write signed transaction to file
-    echo "$signed_txn" | sudo tee $combo_txn_file
+      # Create delegation transaction and save withdrawal message to variable
+      delegation_txn=$(sudo -u gaiad /opt/go/bin/gaiacli tx staking delegate --home=/opt/gaiacli --amount=${total}stake --from=game-of-stake-key-validator4 --validator=cosmosvaloper1844lltc96kxkm5mq03my90se4cdssewmj229m0 --chain-id=game_of_stakes_5 --generate-only --gas=154250)
+      delegation_msg=$(echo $delegation_txn | jq -r '.value.msg[0]')
 
-    # Broadcast the signed transaction
-    echo "Broadcasting transaction... Result:"
-    echo $(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx broadcast $combo_txn_file --account-number 0 --chain-id game_of_stakes_5 --gas=154250 --from=game-of-stake-key-validator4 --trust-node)
+      # Combine the delegation message with the withdrawal transaction to create the final transaction (with both messages)
+      withdrawal_and_delegation_txn=$(echo $withdrawal_txn | jq ".value.msg[1] |= . + $delegation_msg")
 
-    sleep 5m
+      echo "Saving generated withdrawal/delegation transaction to $combo_txn_file..."
+
+      # Write combined transaction to file
+      echo "$withdrawal_and_delegation_txn" | sudo tee $combo_txn_file
+
+      # Sign combined transaction
+      signed_txn=$(echo "${GAIA_KEY}" | sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx sign $combo_txn_file --trust-node --account-number 0 --name game-of-stake-key-validator4 --gas=154250 --chain-id game_of_stakes_5 --from=game-of-stake-key-validator4)
+
+      echo "Transaction signed! Saving signed transaction to $combo_txn_file..."
+
+      # Write signed transaction to file
+      echo "$signed_txn" | sudo tee $combo_txn_file
+
+      # Broadcast the signed transaction
+      echo "Broadcasting transaction... Result:"
+      echo $(sudo -u gaiad /opt/go/bin/gaiacli --home=/opt/gaiacli tx broadcast $combo_txn_file --account-number 0 --chain-id game_of_stakes_5 --gas=154250 --from=game-of-stake-key-validator4 --trust-node)
+
+      sleep 1m
+    fi
   fi
 done
